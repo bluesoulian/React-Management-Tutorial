023bbe7a4e98b762adb358d4bb71387d0614e4b1
bluesoulian <bluesoulian@gmail.com>
2021-06-24 21:44:24 +0800

    yhujh

--- client/src/App.js
+++ client/src/App.js
@@ -21,34 +21,24 @@ const styles = theme =>({
   }
 })
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'http://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '961222',
-    'gender': '남자',
-    'job': '대학생'
-  },
-  {
-    'id': 2,
-    'image': 'http://placeimg.com/64/64/2',
-    'name': '박대성',
-    'birthday': '881218',
-    'gender': '남자',
-    'job': '변호사'
-  },
-  {
-    'id': 3,
-    'image': 'http://placeimg.com/64/64/3',
-    'name': '이진성',
-    'birthday': '880410',
-    'gender': '여자',
-    'job': '교사'
-  }
-]
-
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return(
@@ -63,21 +53,9 @@ class App extends React.Component {
           <TableCell>직업</TableCell>
         </TableHead>
         <TableBody>
-          {
-            customers.map(c => {
-                return (
-                  <Customer
-                      key={c.id}
-                      id={c.id}
-                      image={c.image}
-                      name = {c.name}
-                      birthday = {c.birthday}
-                      gender = {c.gender}
-                      job = {c.job}
-                    />
-                )
-            }) 
-          }
+          {this.state.customers? this.state.customers.map(c => {
+                return ( <Customer key={c.id} id={c.id} image={c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} />)
+            }) : ""} 
         </TableBody>
       </Table>
     </Paper>
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/api/customers', (req,res) => {
+    res.send(
+        [
+            {
+              'id': 1,
+              'image': 'http://placeimg.com/64/64/1',
+              'name': '홍길동',
+              'birthday': '961222',
+              'gender': '남자',
+              'job': '대학생'
+            },
+            {
+              'id': 2,
+              'image': 'http://placeimg.com/64/64/2',
+              'name': '박대성',
+              'birthday': '881218',
+              'gender': '남자',
+              'job': '변호사'
+            },
+            {
+              'id': 3,
+              'image': 'http://placeimg.com/64/64/3',
+              'name': '이진성',
+              'birthday': '880410',
+              'gender': '여자',
+              'job': '교사'
+            }
+          ]
+    )
+} )
+
+app.listen(port, () => console.log(`on ${port}`))
